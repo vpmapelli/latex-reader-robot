732a97c7fb5e5f33e251e8d4128712ca36227928
vinicius <vinicius@vinicius-debian>
2019-09-03 23:43:57 -0300

    cropper: save annots positions in a list

--- robots/cropper.py
+++ robots/cropper.py
@@ -4,33 +4,42 @@ def openFile(filename):
     '''Simple wrapper to open a pdf file'''
     return PyPDF2.PdfFileReader(filename,'rb')
 
-def readPages(reader):
-    '''Read all pages from a pdf file
-    Input: PyPDF2.PdfFileReader object'''
+def readPagesAndSaveAnnotsPositions(reader):
+    '''Input: PyPDF2.PdfFileReader object'''
 
+    fileRectList = []
     for pageNum in range(reader.numPages):
         page = reader.getPage(pageNum)
-        print(pageNum)
-        # print(page)
-        searchPolygonAnnottations(page)
+        pageRectList = searchPolygonAnnottations(page)
+        fileRectList.append(pageRectList)
+    
+    return fileRectList
 
 def searchPolygonAnnottations(page):
     '''Search for polygon annottations in a page'''
+
+    rectList = []
     if '/Annots' in page:
         annottsObjects = page['/Annots'].getObject()
         numOfAnnottations = len(annottsObjects)
         
         for i in range(numOfAnnottations):
-            print(annottsObjects[i].getObject(),"\n")
             if '/Rect' in annottsObjects[i].getObject():
-                print("Found a polygon annotattion!")
+                rectList.append(annottsObjects[i].getObject()['/Rect'])
+        
+    return rectList
 
-
-        print("Found some annottations!\n\n")
-    else:
-        print("No annottations found!\n")
 
 def run(filename):
     '''Run cropper robot'''
     reader = openFile(filename)
-    readPages(reader)
+    fileAnnotsList = readPagesAndSaveAnnotsPositions(reader)
+
+    for i,pageList in enumerate(fileAnnotsList):
+        print("Page number = {}".format(i))
+        if not pageList:
+            print("No annotations found\n")
+        else:
+            for rect in pageList:
+                print("Annotation position: {}".format(rect))
+            print("\n")
